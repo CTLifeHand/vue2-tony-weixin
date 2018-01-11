69a52473bc30ae044b207d4bcdea142066da0346
coderTonyChan <101992315@qq.com>
2018-01-11 11:17:43 +0800

    我的模块 完成第一层框架搭建

--- src/frames/me/me.vue
+++ src/frames/me/me.vue
@@ -2,24 +2,34 @@
     <section>
 		<!-- 头部 -->
 		<Nav logo-part="true" search-part="true" add="true"></Nav>
-    <h1>通讯录</h1>
-    <h2>我</h2>
+		<find-me></find-me>
 		<!-- 底部导航 -->
 		<TarBar></TarBar>
+		<transition name="router-show">
+		    <router-view></router-view>
+		</transition>
 	</section>
 </template>
 
 <script>
 import Nav from '../../components/Nav'
+import findMe from '../../components/Setting'
 import TarBar from '../../components/TarBar'
 export default {
   components: {
     Nav,
+    findMe,
     TarBar
   }
 }
 </script>
 
 <style lang="scss">
-
+	@import "../../style/public";
+	.router-show-enter-active,.router-show-leave-active{
+		transition: all .4s;
+	}
+	.router-show-enter,.router-show-leave-active{
+		transform:translateX(100%)
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,23 @@ const more = r => require.ensure([], () => r(require('../frames/addressbook/deta
 const friendcircle = r => require.ensure([], () => r(require('../frames/find/push/friendcircle')), 'friendcircle')
 const miniapps = r => require.ensure([], () => r(require('../frames/find/push/miniapps')), 'miniapps')
 
+// me
+const personaldetails = r => require.ensure([], () => r(require('../frames/me/personaldetails/personaldetails')), 'personaldetails')
+const photoalbum = r => require.ensure([], () => r(require('../frames/me/photoalbum/photoalbum')), 'photoalbum')
+const collect = r => require.ensure([], () => r(require('../frames/me/collect/collect')), 'collect')
+const wallet = r => require.ensure([], () => r(require('../frames/me/wallet/wallet')), 'wallet')
+const cardbag = r => require.ensure([], () => r(require('../frames/me/cardbag/cardbag')), 'cardbag')
+const settings = r => require.ensure([], () => r(require('../frames/me/settings/settings')), 'settings')
+// me -> settings
+const newmessage = r => require.ensure([], () => r(require('../frames/me/settings/detailset/newmessage')), 'newmessage')
+const disturbance = r => require.ensure([], () => r(require('../frames/me/settings/detailset/disturbance')), 'disturbance')
+const chat = r => require.ensure([], () => r(require('../frames/me/settings/detailset/chat')), 'chat')
+const privacy = r => require.ensure([], () => r(require('../frames/me/settings/detailset/privacy')), 'privacy')
+const currency = r => require.ensure([], () => r(require('../frames/me/settings/detailset/currency')), 'currency')
+const aboutwc = r => require.ensure([], () => r(require('../frames/me/settings/detailset/aboutwc')), 'aboutwc')
+const help = r => require.ensure([], () => r(require('../frames/me/settings/detailset/help')), 'help')
+const login = r => require.ensure([], () => r(require('../frames/me/settings/detailset/login')), 'login')
+
 Vue.use(Router)
 
 const router = new Router({
@@ -73,7 +90,62 @@ const router = new Router({
     {
       path: '/me',
       name: 'me',
-      component: me
+      component: me,
+      children: [
+        {
+          path: '/me/personaldetails', component: personaldetails // 个人信息
+        },
+        {
+          path: '/me/cardbag', component: cardbag // 卡包
+        },
+        {
+          path: '/me/photoalbum', component: photoalbum // 相册
+        },
+        {
+          path: '/me/collect', component: collect // 我的收藏
+        },
+        {
+          path: '/me/wallet', component: wallet // 钱包
+        },
+        {
+          path: '/me/settings',
+          component: settings, // 设置
+          children: [
+            {
+              path: '/me/settings/newmessage',
+              component: newmessage
+            }, // 新消息提醒
+            {
+              path: '/me/settings/disturbance',
+              component: disturbance
+            }, // 勿扰模式
+            {
+              path: '/me/settings/chat',
+              component: chat
+            }, // 聊天
+            {
+              path: '/me/settings/privacy',
+              component: privacy
+            }, // 隐私
+            {
+              path: '/me/settings/currency',
+              component: currency
+            }, // 通用
+            {
+              path: '/me/settings/aboutwc',
+              component: aboutwc
+            }, // 关于微信
+            {
+              path: '/me/settings/help',
+              component: help
+            }, // 帮助与反馈
+            {
+              path: '/me/settings/login',
+              component: login
+            } // 退出
+          ]
+        }
+      ]
     },
     {
       path: '/singlechat',
